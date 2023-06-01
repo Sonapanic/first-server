899dcd102973df7d5936ed87a5a0d0c203f1d2ab
Isaac Summers <monogator@gmail.com>
2023-06-01 11:30:59 -0700

    test commit

---
+++ helloServer.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let http = require('http')
+let port = process.env.PORT || 8000
+
+let server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World')
+})
+
+server.listen(port, () => {
+    console.log('Listening on port', port)
+})
